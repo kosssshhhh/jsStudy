6595be16bf0bcb7d12e614a4741c8b44408245fc
kosssshhhh <kosukwhon@gmail.com>
2021-10-19 20:44:12 +0900

    function

---
+++ app.js
@@ -0,0 +1,9 @@
+const player = {
+    name: "nico",
+    sayHello: function(otherPersonsName){
+        console.log("hello! " + otherPersonsName + " nice to meet you");
+    },
+};
+
+console.log(player.name);
+player.sayHello("kosh");
